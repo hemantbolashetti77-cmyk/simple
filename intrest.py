ec1a8f80f853f06c4aa5e3656765a09f4e3285fa
hemant17 <hemantbolashetti77@gmail.com>
2025-10-23 16:15:47 +0530

    Update intrest.py

--- intrest.py
+++ intrest.py
@@ -3,3 +3,7 @@ R = float(input("Enter Rate of Interest: "))
 T = float(input("Enter Time (in years): "))
 
 SI = (P * R * T) / 100
+CI = P * (pow((1 + R / 100), T)) - P
+
+print("Simple Interest: ₹", SI)
+print("Compound Interest: ₹", round(CI, 2))
